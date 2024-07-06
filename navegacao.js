f203b0c5596319ca21c70e30d91326b1a0cb1bba
BrunaGomesTW <bruna.camilo@thoughtworks.com>
2024-07-06 09:31:09 -0300

    finished

--- navegacao.js
+++ navegacao.js
@@ -11,23 +11,23 @@
 function exibeArtigo(idDoArtigo) {
   // itera sobre todos os artigos (.aba-conteudo) e os desativa
   document
-    .querySelectorAll('.aba-conteudo')
-    .forEach(el => el.classList.remove('ativa'));
+    .querySelectorAll(".aba-conteudo")
+    .forEach((el) => el.classList.remove("ativa"));
 
   // exibe o artigo que tem o id do link clicado
   const artigoParaExibirEl = document.querySelector(idDoArtigo);
-  artigoParaExibirEl.classList.add('ativa');
+  artigoParaExibirEl.classList.add("ativa");
 }
 
 // busca e itera sobre todos os itens do menu ("gravitação",
 // "osiris-rex" e "sobre aliens") para tratar do evento de clique em cada um
-const itensDoMenu = document.querySelectorAll('#menu-principal a');
-itensDoMenu.forEach(el => {
-  el.addEventListener('click', evento => {
+const itensDoMenu = document.querySelectorAll("#menu-principal a");
+itensDoMenu.forEach((el) => {
+  el.addEventListener("click", (evento) => {
     // descobre quem foi clicado e qual era o href do link e para que id
     // ele apontava
     const hrefDoLink = evento.target.href;
-    const idApontadoPeloLink = hrefDoLink.substr(hrefDoLink.lastIndexOf('#'));
+    const idApontadoPeloLink = hrefDoLink.substr(hrefDoLink.lastIndexOf("#"));
 
     exibeArtigo(idApontadoPeloLink);
   });
@@ -37,7 +37,7 @@ itensDoMenu.forEach(el => {
 // página. Em caso afirmativo, se for #aliens ou #philae, ativa as suas
 // respectivas abas
 if (location.hash) {
-  const hrefDosItensDeMenu = Array.from(itensDoMenu).map(el => el.href);
+  const hrefDosItensDeMenu = Array.from(itensDoMenu).map((el) => el.href);
   if (hrefDosItensDeMenu.indexOf(location.hash)) {
     // exibe o artigo que tem o id do link clicado
     exibeArtigo(location.hash);
